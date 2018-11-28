fee565e7c4e51fd42bf874b9795b417fedfe4cd3
des-tinyDM <destinyleaross@gmail.com>
2018-11-27 22:41:02 -0500

    GraphQL Yoga server running and connected to Prisma db

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    console.log(item);
+    return item;
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  // if the query is exactly the same, this forwards the query from prisma to the Yoga server. All you need to do is write the following line:
+  items: forwardTo("db")
+  // async items(parent, args, ctx, info) {
+  //   console.log("Getting Items!!");
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
